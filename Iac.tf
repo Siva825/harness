05bb91199a11c631f525758bbef4524bf509e33b
Siva825 <gsp200126@gmail.com>
2025-12-09 11:27:12 +0530

    Update Iac.tf

--- Iac.tf
+++ Iac.tf
@@ -2,12 +2,12 @@ provider "google" {
     project = "siva-477505" 
 }
 resource "google_compute_instance" "instance1" {
-    name = "vm-1"
-    zone =  "us-west1-b" 
-    machine_type = "e2-micro"
+    name = "all-in-one"
+    zone =  "us-central1-c" 
+    machine_type = "e2-medium "
     boot_disk {
       initialize_params {
-        image = "debian-cloud/debian-12"        
+        image = "projects/ubuntu-os-cloud/global/images/ubuntu-2404-noble-amd64-v20251205"        
       }
     }
     network_interface {
